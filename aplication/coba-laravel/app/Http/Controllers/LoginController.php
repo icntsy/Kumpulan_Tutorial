6ce3234fb4bf646e7ebbc684eea7c32908dd7332
icntsy <icanatasya72@gmail.com>
2022-10-26 16:23:42 +0700

    create

--- aplication/coba-laravel/app/Http/Controllers/LoginController.php
+++ aplication/coba-laravel/app/Http/Controllers/LoginController.php
@@ -21,9 +21,18 @@ class LoginController extends Controller
 
         if(Auth::attempt($credentials)) {
             $request->session()->regenerate();
-            return redirect()->intended('/dasahboard');
+            return redirect()->intended('/dashboard');
         }
-
         return back()->with('loginError', ' Login ');
+    }
+
+    public function logout(Request $request){
+        Auth::logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/');
     }
 }
--- aplication/coba-laravel/resources/views/partials/navbar.blade.php
+++ aplication/coba-laravel/resources/views/partials/navbar.blade.php
@@ -7,23 +7,47 @@
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a class="nav-link {{ ($title === "home") ? 'active' : '' }}" href="/">Home</a>
+            <a class="nav-link {{ Request::is('') ? 'active' : '' }}" href="/">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link {{ ($title === "about") ? 'active' : '' }}" href="/about">About</a>
+            <a class="nav-link {{ Request::is('about') ? 'active' : '' }}" href="/about">About</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link {{ ($title === "posts") ? 'active' : '' }}" href="/posts">Blog</a>
+            <a class="nav-link {{ Request::is('posts') ? 'active' : '' }}}" href="/posts">Blog</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link {{ ($title === "categories") ? 'active' : '' }}" href="/categories">Categories</a>
+            <a class="nav-link {{ Request::is('categories') ? 'active' : '' }}" href="/categories">Categories</a>
           </li>
         </ul>
+
+
+
         <ul class="navbar-nav ms-auto">
-          <li class="nav-item">
-            <a href="/login" class="nav-link"><i class="bi bi-box-arrow-in-right"></i> Login</a>
-          </li>
-        </ul>
+            @auth
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Welcome back, {{ auth()->user()->name }}
+                    </a>
+                    <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="/dashboard"><i class="bi bi-card-list"></i>My Dashboard</a></li>
+
+                    <li><hr class="dropdown-divider"></li>
+                    <li>
+                        <form action="/logout" method="post">
+                            @csrf
+                            <button type="submit" class="dropdown-item"><i class="bi bi-box-arrow-right"></i>Logout
+                            </button>
+                        </form>
+                    </ul>
+                </li>
+            @else
+            <li class="nav-item">
+              <a href="/login" class="nav-link"><i class="bi bi-box-arrow-in-right"></i> Login</a>
+            </li>
+            @endauth
+          </ul>
+
+
       </div>
     </div>
   </nav>
