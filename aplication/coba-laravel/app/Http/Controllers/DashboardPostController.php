434748fd2a4c8f44e5f288158b89aa2d83913e7b
icntsy <icanatasya72@gmail.com>
2022-11-28 14:37:59 +0700

    laporan pmi

--- aplication/coba-laravel/app/Http/Controllers/DashboardPostController.php
+++ aplication/coba-laravel/app/Http/Controllers/DashboardPostController.php
@@ -6,6 +6,8 @@ use App\Models\Post;
 use App\Models\Category;
 use Illuminate\Http\Request;
 use \Cviebrock\EloquentSluggable\Services\SlugService;
+use Illuminate\Support\Str;
+
 class DashboardPostController extends Controller
 {
     /**
@@ -41,7 +43,17 @@ class DashboardPostController extends Controller
      */
     public function store(Request $request)
     {
-        return $request;
+     $validateData =  $request->validate([
+        'title' =>'required|max:255',
+        'slug' => 'required|unique:posts',
+        'category_id' =>'required',
+        'body' => 'required'
+     ]);
+
+     $validateData['user_id'] = auth()->user()->id;
+     $validateData['excerpt'] = Str::limit(strip_tags($request->body), 200);
+        Post::create($validateData);
+        return redirect('dashboard/posts')->with('success', 'New post has been added!');
     }
 
     /**
@@ -65,7 +77,10 @@ class DashboardPostController extends Controller
      */
     public function edit(Post $post)
     {
-        //
+        return view('dashboard.posts.edit', [
+            'post' => $post,
+            'categories'=> Category::all()
+        ]);
     }
 
     /**
@@ -88,7 +103,8 @@ class DashboardPostController extends Controller
      */
     public function destroy(Post $post)
     {
-        //
+        Post::destroy($post->id);
+    return redirect('dashboard/posts')->with('success', 'Post has been added!');
     }
 
     public function checkSlug(Request $request){
